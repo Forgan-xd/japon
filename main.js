909be5cdee01c4006dd0c11aa3938c0ca17ccd6a
tafur <davidtafur07@hotmail.com>
2020-10-09 17:59:27 -0500

    commit

---
+++ main.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.general');
+ScrollReveal().reveal('.primeraimagen',{delay:500})
+ScrollReveal().reveal('.primerparrafo',{delay:500})
+ScrollReveal().reveal('.segundoparrafo',{delay:500})
+ScrollReveal().reveal('.imagen2',{delay:500})
+ScrollReveal().reveal('.redes sociales',{delay:1000})
+function botonredes(){
+    document.getElementById("redessocialiticas").style.display="block",ScrollReveal().reveal("redessocialiticas")
+}
+
+
